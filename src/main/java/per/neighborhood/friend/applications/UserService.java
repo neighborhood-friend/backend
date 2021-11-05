04a9d60b4b759050bbf872c53533bdb345a53d04
ddu0422 <43198414+ddu0422@users.noreply.github.com>
2021-11-05 23:20:18 +0900

    refactor(user): 사용자 상세 정보 DB 분리 (#7)
    * refactor: separate user detail information
    
    * chore: change image url length limit

--- src/main/java/per/neighborhood/friend/applications/UserService.java
+++ src/main/java/per/neighborhood/friend/applications/UserService.java
@@ -3,8 +3,12 @@ package per.neighborhood.friend.applications;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import per.neighborhood.friend.client.dto.KakaoRegisterResponse;
 import per.neighborhood.friend.domain.User;
+import per.neighborhood.friend.domain.UserDetail;
+import per.neighborhood.friend.domain.UserDetailRepository;
 import per.neighborhood.friend.domain.UserRepository;
 
 @Service
@@ -13,20 +17,28 @@ import per.neighborhood.friend.domain.UserRepository;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final UserDetailRepository userDetailRepository;
 
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void register(KakaoRegisterResponse kakaoRegisterResponse) {
         if (userRepository.existsByAuthId(kakaoRegisterResponse.getId())) {
             return;
         }
 
-        userRepository.save(
-            User.builder()
-                .authId(kakaoRegisterResponse.getId())
-                .email(kakaoRegisterResponse.getKakaoAccount().getEmail())
+        UserDetail userDetail = userDetailRepository.save(
+            UserDetail.builder()
                 .nickname(kakaoRegisterResponse.getKakaoAccount().getProfile().getNickname())
                 .thumbnailImageUrl(kakaoRegisterResponse.getKakaoAccount().getProfile().getThumbnailImageUrl())
                 .profileImageUrl(kakaoRegisterResponse.getKakaoAccount().getProfile().getProfileImageUrl())
                 .isDefaultImage(kakaoRegisterResponse.getKakaoAccount().getProfile().isDefaultImage())
+                .build()
+        );
+
+        userRepository.save(
+            User.builder()
+                .authId(kakaoRegisterResponse.getId())
+                .email(kakaoRegisterResponse.getKakaoAccount().getEmail())
+                .userDetail(userDetail)
                 .build()
         );
     }
