1e9e191eb5ba66b001f94cd173f92603cd106729
ddu0422 <43198414+ddu0422@users.noreply.github.com>
2021-11-17 23:51:42 +0900

    feat(user): add response property (#13)

--- src/main/java/per/neighborhood/friend/applications/dto/UserDetailResponse.java
+++ src/main/java/per/neighborhood/friend/applications/dto/UserDetailResponse.java
@@ -1,5 +1,6 @@
 package per.neighborhood.friend.applications.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
@@ -8,7 +9,12 @@ import lombok.*;
 @Builder
 @Getter
 @ToString
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class UserDetailResponse {
     @JsonProperty("nickname")
     private String nickname;
+    @JsonProperty("thumbnailImageUrl")
+    private String thumbnailImageUrl;
+    @JsonProperty("profileImageUrl")
+    private String profileImageUrl;
 }
